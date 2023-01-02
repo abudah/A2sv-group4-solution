edf0c7ab22a78ad96ec594c7924edd7777f39925
Fekede Alemayehu <fekedealemayehu4@gmail.com>
2023-01-02 23:27:30 +0300

    E. Trafic light question

--- Contests/Jan 1, 2023/E. Trafic light.py
+++ Contests/Jan 1, 2023/E. Trafic light.py
@@ -4,8 +4,21 @@ for i in range(n):
         args=input().split()
         colors=input()
         val=colors.index(args[1])
-        on.append(colors.index('g'))
-                        
+        colors+=colors
+        curr=0
+        distances=0
+        nearest=0
+        targetFound=False
+        while(curr<len(colors)):
+                if  colors[curr]==args[1]:
+                        targetFound=True
+                if  targetFound:
+                        if colors[curr]=='g':
+                                distances=max(distances,nearest)
+                                nearest=0
+                                targetFound=False
+                        else:
+                                nearest+=1
+                curr+=1
+        print(distances)
 
-for i in on:
-        print(i)
